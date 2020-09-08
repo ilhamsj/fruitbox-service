ab1a3ae76da7ef01be0c95c82d3308d5a49b018e
ilhamsj <ilhamsaputrajati@gmail.com>
2020-09-08 13:14:37 +0700

    add response

--- app/Http/Controllers/V1/PostController.php
+++ app/Http/Controllers/V1/PostController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Traits\JsonResponse;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    use JsonResponse;
+
+    public function __construct(Post $post)
+    {
+        $this->posts = $post;
+    }
+    
     public function index()
     {
-        $posts = Post::all();
-        return response()->json($posts);
+        $posts = $this->posts->all();
+        return $this->responseWithCondition($posts, 'Data successfully retrieved', 'Failed to retrieve data');
     }
 
     public function store(Request $request)
@@ -21,14 +29,14 @@ class PostController extends Controller
             'description' => 'required:15',
         ]);
 
-        $post = Post::create($request->all());
-        return response()->json($post);
+        $post = $this->posts->store($request->all());
+        return $this->responseWithCondition($post, 'Data successfully stored', 'Failed to store data');
     }
 
     public function show($id)
     {
-        $post = Post::find($id);
-        return response()->json($post);
+        $posts = $this->posts->find($id);
+        return $this->responseWithCondition($posts, 'Data successfully retrieved', 'Failed to retrieve data');
     }
 
     public function update(Request $request, $id)
@@ -38,15 +46,15 @@ class PostController extends Controller
             'description' => 'required:15',
         ]);
         
-        $post = Post::find($id);
-        $post->update($request->all());
+        $post = $this->posts->find($id);
+        $post_update = $post->update($request->all());
 
-        return response()->json($post);
+        return $this->responseWithCondition($post_update, 'Data successfully updated', 'Failed to update data');
     }
 
     public function destroy($id)
     {
-        $post = Post::destroy($id);
-        return response()->json($post);
+        $post = $this->posts->destroy($id);
+        return $this->responseWithCondition($post, 'Data successfully deleted', 'Failed to delete data');
     }
 }
