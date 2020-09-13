3476bd3fb9c20589cc64627a08c6e6324e1f5850
ilhamsj <ilhamsaputrajati@gmail.com>
2020-09-13 20:03:18 +0700

    add user address

--- database/migrations/2020_09_13_125108_user_addresses.php
+++ database/migrations/2020_09_13_125108_user_addresses.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Stores extends Migration
+class UserAddresses extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class Stores extends Migration
      */
     public function up()
     {
-        Schema::create('stores', function (Blueprint $table) {
+        Schema::create('user_addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('category_id');
             $table->bigInteger('village_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('address');
-            $table->foreign('category_id')->references('id')->on('categories');
             $table->foreign('village_id')->references('id')->on('villages');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class Stores extends Migration
      */
     public function down()
     {
-        Schema::drop('stores');
+        Schema::drop('user_addresses');
     }
 }
